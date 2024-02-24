bfedd6690e002bc060d1c6b3b00b2432f142e7d5
TALHA HAROON <152915208+talha765haroon@users.noreply.github.com>
2024-02-25 03:41:03 +0500

    Add files via upload

---
+++ q11.js
@@ -0,0 +1,9 @@
+"use strict";
+/* Ques # 11:
+Names: store the name of a few of your friends in a array called names, print each person's name by accessing each element in the list, one at a time.*/
+let names = ["Ali", "Hussain", "Zohaib", "Sohail"];
+console.log(names);
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
---
+++ q11.ts
@@ -0,0 +1,11 @@
+/* Ques # 11:
+Names: store the name of a few of your friends in a array called names, print each person's name by accessing each element in the list, one at a time.*/
+
+
+let names : string[] =["Ali","Hussain","Zohaib","Sohail"]
+
+console.log(names);
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
